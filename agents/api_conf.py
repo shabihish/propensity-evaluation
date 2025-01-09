8f46d13ca0b3590a73268b613e4ccc21351cd260
Shayan Shabihi <shabihish@gmail.com>
2025-01-09 16:32:54 -0500

    Initial commit

---
+++ agents/api_conf.py
@@ -0,0 +1,6 @@
+class APIConfiguration:
+    def __init__(self, model_name, model_provider, api_base, use_cache):
+        self.model_name = model_name
+        self.model_provider = model_provider
+        self.api_base = api_base
+        self.use_cache = use_cache
