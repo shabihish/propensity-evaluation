4b06647b8d5ec009cec558f9e601679317ea2fd9
Shayan Shabihi <shabihish@gmail.com>
2025-09-29 00:09:24 -0400

    Remove a bunch of unnecessary files

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="my_simple_agent_package",  # You can change this to your desired package name
-    version="0.1.0",                # A starting version
-    packages=find_packages(),       # This will automatically find the 'agent' package
-)
